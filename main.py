1cad799e5124e67659f6ef664ddb72d82717baf4
Shahibur Rahaman <72332158+Shahibur50@users.noreply.github.com>
2021-03-28 18:50:02 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+
+# Type as much fizzing rules you want ;-)
+fizzing_rules = {
+    3: "Fizz",
+    5: "Buzz",
+    7: "Fuzz",
+    11: "Bizz",
+    13: "Biff"
+}
+
+
+def main(dictionary):
+    for i in range(1, int(input("Range: ")) + 1):
+        output = ""
+
+        for number, value in dictionary.items():
+            if i % number == 0:
+                output += value
+        if output == "":
+            output = i
+
+        print(output)
+
+
+if __name__ == "__main__":
+    main(fizzing_rules)
+
